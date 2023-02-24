4b6e92e04e59ec09d786d3dbb73d07349d6d8115
chudamani15 <109755887+chudamani15@users.noreply.github.com>
2023-02-24 22:54:39 +0530

    Create Day_8_Caesar_Cipher.py

---
+++ Day_8_Caesar_Cipher.py
@@ -0,0 +1,58 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+            'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(logo)
+
+def coding(direction, input_text, shift_number):
+
+    output_text = ""
+
+    for char in input_text:
+        if char in alphabet:
+            b = alphabet.index(char)
+            letter = alphabet[b + shift_number]
+            output_text += letter
+        else:
+            output_text += char
+
+    print(f'Here is the {direction}d result: {output_text}')
+
+game_on = True
+while game_on:
+    method = input("Type 'encode' to encrypt. Type 'decode' to decrypt:\n")
+    message = input("Type your message:\n")
+    shift = int(input("Type the shift number:\n"))
+
+    shift %= 26
+
+    if method == 'encode':
+        coding(method, message, shift)
+
+    elif method == 'decode':
+        coding(method, message, -shift)
+
+    else:
+        print('Type correct method.')
+
+    game = input("Type 'yes' if you want to go again or type 'no'.\n")
+    if game == 'no':
+        game_on = False
